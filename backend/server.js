d27c443406a537b04bf43f54f248752ab867497f
Eric Wong <ericwong12@live.com>
2020-04-11 13:37:41 -0600

    updating server

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const apiRouting = require("./routes/apiRouting");
+const path = require("path")
 require("dotenv").config();
 
 //Express server
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+const publicPath = path.join(__dirname, "..", "public")
+
 //Middleware
 app.use(morgan("short"));
 app.use(cors());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
+app.use(express.static(publicPath));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
@@ -27,6 +31,9 @@ connection.once("open", () => {
     console.log("Connection to MongoDB was successful!")
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 app.use("/", apiRouting);
 
 //Starts the server
